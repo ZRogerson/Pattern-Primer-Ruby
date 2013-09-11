9b6f1e153f6c406c7063fe0aab12b90e49b64ede
Zac Rogerson <zac@enmasse.com>
2013-09-11 14:20:21 -0700

    working towards a single page layout switch

--- public/app.js
+++ public/app.js
@@ -1,13 +1,25 @@
 $(document).ready(function(){
-  var links = $('#nav').find('li'),
+  var links = $('#menu').find('li'),
   block;
 
   $.each(links, function(){
     $(this).bind('click', function(e){
+      $(links).removeClass('active');
+      $(this).addClass('active');
       e.preventDefault();
       block = $(this).find('a').attr('data-name');
       $('.pattern-right').css('display', 'none');
       $('#pattern-' + block).css('display', 'block');
     })
+  })
+  $('#pattern-home').css('display', 'block');
+
+  $('#controls #toggle-nav').bind('click', function(){
+    if ( !$(this).hasClass('active') ) {
+      location.reload();
+    }
+    $('body').removeClass('nav');
+    $('.pattern-right').css('display', 'block');
+    $(this).removeClass('active');
   })
 })
